587e421147e3b148906fa8375c0e22c80b345945
Divya <beingdivyadixit@gmail.com>
2022-12-12 00:04:35 +0530

    Constructor

---
+++ ConsoleApp2/Constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Constructor
+    {  
+         public Constructor()
+        {
+            Console.WriteLine("Default constructor");
+        }
+        //Paramitrized constructor
+        public Constructor(int Phone)
+        {
+            Console.WriteLine("Paramitrized constructor");
+        }
+        // Local variable - Scope is only to method
+        public static void StudentAge(int phoneNumber)
+        {
+            int age = 0;
+            age = age + 10;
+            Console.WriteLine("Student age is {0} and phonenumber is {1}:",age,phoneNumber);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    
+            Constructor.StudentAge(147663686);
+            Constructor constructor= new Constructor();
+            Constructor constructor2= new Constructor(546);
+            
+        }
+    }
+}
