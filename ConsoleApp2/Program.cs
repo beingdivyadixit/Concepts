731c523c64e2d2d633adb37a178308d9eefd56d6
Divya <beingdivyadixit@gmail.com>
2022-12-12 00:23:21 +0530

    Updated to static variable concept

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,16 +9,19 @@ namespace ConsoleApp2
             //Constructor.StudentAge(147663686);
             //Constructor constructor= new Constructor();
             //Constructor constructor2= new Constructor(546);
-            Instantce marks= new Instantce();
-            marks.engMarks = 85;
-            marks.mathsMarks = 98;
-            marks.phyMarks = 90;
-            marks.MarksDetails();
-            Instantce marks1= new Instantce();
-            marks1.engMarks = 80;
-            marks1.mathsMarks = 95;
-            marks1.phyMarks = 88;
-            marks1.MarksDetails();
+           // Instantce marks= new Instantce();
+            //marks.engMarks = 85;
+            //marks.mathsMarks = 98;
+            //marks.phyMarks = 90;
+            //marks.MarksDetails();
+            //Instantce marks1= new Instantce();
+            //marks1.engMarks = 80;
+            //marks1.mathsMarks = 95;
+            //marks1.phyMarks = 88;
+            //marks1.MarksDetails();
+            Static.number = 13;
+            Console.WriteLine(Static.number);
+            Console.ReadLine();
             
         }
     }
