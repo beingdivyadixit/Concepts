06b3711bbc3ac74de73a1b572d5be7424d11ddbd
Divya <beingdivyadixit@gmail.com>
2022-12-12 00:42:11 +0530

    Updated to Array Concept

---
+++ ConsoleApp2/Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Array
+    {
+        public static void TestArray()
+        {
+            Console.WriteLine("Please Enter size of Array");
+            int size=Convert.ToInt32(Console.ReadLine());
+            int[] numbers= new int[size];
+            Console.WriteLine("Reading input from Users and storing in an array");
+            for (int i=0; i<size; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());           
+            }
+            Console.WriteLine("Displaying array elements");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i] + " ");
+            }
+            Console.WriteLine("/n Updating value of 3rd element please enter the number");
+            numbers[2]=Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]+" ");
+            }
+            
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,11 +5,11 @@ namespace ConsoleApp2
     internal class Program
     {
         static void Main(string[] args)
-        {    
+        {
             //Constructor.StudentAge(147663686);
             //Constructor constructor= new Constructor();
             //Constructor constructor2= new Constructor(546);
-           // Instantce marks= new Instantce();
+            // Instantce marks= new Instantce();
             //marks.engMarks = 85;
             //marks.mathsMarks = 98;
             //marks.phyMarks = 90;
@@ -19,9 +19,10 @@ namespace ConsoleApp2
             //marks1.mathsMarks = 95;
             //marks1.phyMarks = 88;
             //marks1.MarksDetails();
-            Static.number = 13;
-            Console.WriteLine(Static.number);
-            Console.ReadLine();
+            // Static.number = 13;
+            //Console.WriteLine(Static.number);
+            //Console.ReadLine();
+            Array.TestArray();
             
         }
     }
