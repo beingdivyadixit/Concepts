a8685e76d05eb901af81f11d9c5fc5c8f3c630f5
Divya <beingdivyadixit@gmail.com>
2022-12-12 00:16:16 +0530

    Updated to Instance Concept

---
+++ ConsoleApp2/Instantce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Instantce
+    {
+        public int engMarks;
+        public int mathsMarks;
+        public int phyMarks;
+        public void MarksDetails()
+        {
+            Console.WriteLine("English:{0} Maths:{1} Physics:{2}", engMarks, mathsMarks, phyMarks);
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,9 +6,19 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {    
-            Constructor.StudentAge(147663686);
-            Constructor constructor= new Constructor();
-            Constructor constructor2= new Constructor(546);
+            //Constructor.StudentAge(147663686);
+            //Constructor constructor= new Constructor();
+            //Constructor constructor2= new Constructor(546);
+            Instantce marks= new Instantce();
+            marks.engMarks = 85;
+            marks.mathsMarks = 98;
+            marks.phyMarks = 90;
+            marks.MarksDetails();
+            Instantce marks1= new Instantce();
+            marks1.engMarks = 80;
+            marks1.mathsMarks = 95;
+            marks1.phyMarks = 88;
+            marks1.MarksDetails();
             
         }
     }
